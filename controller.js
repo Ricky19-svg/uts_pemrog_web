81e72e32ec4e379b3bf9d331b30aca6a813255b2
Ricky <rickyarddianto@outlook.com>
2020-05-01 20:06:42 +0700

    berhasil menampilkan data sparepart

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req, res){
-    response.ok("Aplikasi REST API ku berjalan yey!",res)
+    response.ok("Aplikasi REST API ku berjalan!",res)
+};
+
+//menampilkan data sparepart dan montir
+exports.tampildatasparepart = function(req,res){
+    connection.query('SELECT * FROM t_sparepart', function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
 };
