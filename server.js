b67a4349385591ed7c550c4559aff20e5d793419
Ricky <rickyarddianto@outlook.com>
2020-05-16 11:59:16 +0700

    berhasil tampil service

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 var morgan = require('morgan');
 const app = express();
 
+
 //parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -16,6 +17,6 @@ routes(app);
 //daftarkan menu routes dari index
 app.use('/auth', require('./middleware'));
 
- app.listen(3000,() => {
-     console.log('Server Started on port 3000');
- });
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
